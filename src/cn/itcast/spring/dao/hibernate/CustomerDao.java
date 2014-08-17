081f2ea2c9de5a33a121f39f71565481a5fbb2b5
winingos <wangningos@sina.com>
2014-08-17 16:40:46 +0800

    直接注入sf，如果使用sf.opensession()方法就无法再service层面实现事务管理，所以应该使用sf.getCurrentSession()方法。

--- src/cn/itcast/spring/dao/hibernate/CustomerDao.java
+++ src/cn/itcast/spring/dao/hibernate/CustomerDao.java
@@ -1,60 +1,58 @@
 package cn.itcast.spring.dao.hibernate;
 
-import java.sql.SQLException;
 import java.util.List;
 
-import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.springframework.orm.hibernate3.HibernateCallback;
-import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.hibernate.SessionFactory;
 
 import cn.itcast.spring.domain.Customer;
 @SuppressWarnings("unchecked")
 public class CustomerDao implements ICustomerDao {
-	//hibernate模板,封装样板代码
-	private HibernateTemplate ht;
-	public void setHt(HibernateTemplate ht) {
-		this.ht = ht;
-	}
-
+private SessionFactory sf;
+public void setSf(SessionFactory sf) {
+	this.sf = sf;
+}
 @Override
 public void insertCus(Customer c){
-	 ht.save(c);
+	 sf.getCurrentSession().save(c);
  }
 @Override
 public void updateCus(Customer c){
-	ht.update(c);
+	sf.getCurrentSession().update(c);
 }
 
 	public List<Customer> queryById(int id) {
 		String hql="from Customer c where c.id = ?";
-		return ht.find(hql, id);
+		Session ss=sf.getCurrentSession();
+		Query qq =ss.createQuery(hql);
+		qq.setInteger(0, id);
+		return qq.list();
 	}
 
 	
 	@Override
 	public List<Customer> queryAll() {
 		String hql="from Customer";
-		return ht.find(hql);
+		return sf.getCurrentSession().createQuery(hql).list();
 	}
 //  在dao层面进行实务的管理
 	public void updateBat(final List<Customer> lc) {
 //		接口回调
-		ht.execute(new HibernateCallback() {
-			
-			@Override
-			public Object doInHibernate(Session session) throws HibernateException,
-					SQLException {
-//				显式开启实务，实现事务管理。
-				Transaction  tx =session.beginTransaction();
-				for (Customer c : lc) {
-					session.save(c);
-				}
-				tx.commit();
-				return null;
-			}
-		});
+//		ht.execute(new HibernateCallback() {
+//			
+//			@Override
+//			public Object doInHibernate(Session session) throws HibernateException,
+//					SQLException {
+////				显式开启实务，实现事务管理。
+//				Transaction  tx =session.beginTransaction();
+//				for (Customer c : lc) {
+//					session.save(c);
+//				}
+//				tx.commit();
+//				return null;
+//			}
+//		});
 		
 	}
 }
